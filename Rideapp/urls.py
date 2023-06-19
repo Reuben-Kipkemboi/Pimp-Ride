273341301c829def18796e38aa33155418ba2546
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-06-19 10:53:24 +0300

    st:templates - add navbar.html

--- Rideapp/urls.py
+++ Rideapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, request, render
+from django.urls import path
 from . import views
 
 urlpatterns=[
--- Rideapp/views.py
+++ Rideapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import * 
+
+
 
 # Create your views here.
 def index(request):
