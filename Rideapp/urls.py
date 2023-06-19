91bc3a562999f8bc63d7518665556f8059ef45ed
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-06-19 12:29:35 +0300

    st:navbar - optimize navbar styling

--- Rideapp/urls.py
+++ Rideapp/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    # home url
         path('', views.index, name='home'),
+        
+    #customs url
+    
+    
+    #products url
+        
+        
 
 ]
